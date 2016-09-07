e3a86f1b79ebd3be07039785a81e9070aa07002b
Rimian Perkins <hello@rimian.com.au>
2016-09-07 11:26:35 +1000

    disable functionality

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,9 +1,9 @@
-(function() {
-  if (Discourse.dialect_deprecated) { return; }
+(function(enabled) {
+  if (Discourse.dialect_deprecated || !enabled) { return; }
 
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/discordapp\.com\/.+/i);
-})();
+})(Discourse.SiteSettings.whitelist_iframe_plugin_enabled);
