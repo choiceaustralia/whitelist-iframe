b9ba9aef7929347a10e82c5e97c6c4e9da35a59a
Rimian Perkins <hello@rimian.com.au>
2016-09-07 10:34:44 +1000

    toggle plugin enabled with site settings

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,7 @@
 # authors: scossar
 
 # whitelist raw iframes posted by users
+
+enabled_site_setting :whitelist_iframe_plugin_enabled:
+
 register_asset 'javascripts/iframe-whitelist.js', :server_side
